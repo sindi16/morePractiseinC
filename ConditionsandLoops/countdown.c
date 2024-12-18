5ef048773c117d58326148acf78f0b7d7458e379
sindi16 <10029@holbertonstudents.com>
2024-12-18 17:51:02 +0000

    count down to 0

---
+++ ConditionsandLoops/countdown.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int count;
+    for (count = 10; count >= 0; count--)
+    {
+        printf("%d\n", count);
+    }
+    printf("Happy Eid al-Fitr\n");
+    return (0);
+}
