467a7353f7b14d5dadb9d68336cbcdface18d264
sindi16 <10029@holbertonstudents.com>
2024-12-18 15:18:29 +0000

    not complete the checking of password

--- ConditionsandLoops/checkpassword.c
+++ ConditionsandLoops/checkpassword.c
@@ -2,12 +2,12 @@
 
 int main()
 {
-    char password[15];
-    char correct_password[] = "userpassword"
+    char* password[15];
+    char* correct_password[] = "1234"
     printf("Please enter your password: ");
-    scanf("%d", &x);
+    scanf("%s", &password);
 
-    if (x )
+    if (password[0] == '1' && password[1] == '2' && password[2] == '3' && password[3] == '4' && password[4] == '\0')
     {
         printf("Access granted\n");
     }
